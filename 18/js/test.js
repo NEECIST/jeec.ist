656ac675ffc503d7a536f8a7f833fc0f1f07a9bf
NEECIST <admin@neecist.org>
2018-06-05 02:05:58 +0100

    Initial commit

---
+++ 18/js/test.js
@@ -0,0 +1,69 @@
+
+
+
+$('.toggle0').click(function() {
+    $('#target0').slideToggle('slow');
+    $('#target1').hide(500);
+    $('#target1').hide(500);
+    $('#target2').hide(500);
+    $('#target3').hide(500);
+    $('#target4').hide(500);
+    
+});
+
+$('.toggle').click(function() {
+    $('#target').slideToggle('slow');
+    $('#target0').hide(500);
+
+    $('#target1').hide(500);
+    $('#target2').hide(500);
+    $('#target3').hide(500);
+    $('#target4').hide(500);
+    
+});
+
+
+
+$('.toggle1').click(function() {
+    $('#target1').slideToggle('slow');
+    $('#target0').hide(500);
+    
+    $('#target').hide(500);
+    $('#target2').hide(500);
+    $('#target3').hide(500);
+    $('#target4').hide(500);
+});
+
+
+$('.toggle2').click(function() {
+    $('#target2').slideToggle('slow');
+    $('#target0').hide(500);
+    
+    $('#target').hide(500);
+    $('#target1').hide(500);
+    $('#target3').hide(500);
+    $('#target4').hide(500);
+});
+
+
+$('.toggle3').click(function() {
+    $('#target3').slideToggle('slow');
+    $('#target0').hide(500);
+    
+    $('#target').hide(500);
+    $('#target1').hide(500);
+    $('#target2').hide(500);
+    $('#target4').hide(500);
+});
+
+
+
+$('.toggle4').click(function() {
+    $('#target4').slideToggle('slow');
+    $('#target0').hide(500);
+    
+    $('#target').hide(500);
+    $('#target1').hide(500);
+    $('#target2').hide(500);
+    $('#target3').hide(500);
+});
